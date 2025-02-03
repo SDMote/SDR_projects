de90f01ed4466a506f9ad4bfe27f5efba19c8d96
Zimrahin <diegobadillosj@gmail.com>
2025-02-03 16:12:05 +0100

    GRC file to measure once with the SDR now exits automatically Removed QT GUI

--- capture_nRF/capture_nRF.py
+++ capture_nRF/capture_nRF.py
@@ -8,8 +8,6 @@
 # Title: capture_nRF
 # GNU Radio version: v3.11.0.0git-843-g6b25c171
 
-from PyQt5 import Qt
-from gnuradio import qtgui
 from gnuradio import blocks
 from gnuradio import filter
 from gnuradio.filter import firdes
@@ -17,7 +15,6 @@ from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
@@ -26,44 +23,18 @@ import threading
 
 
 
-class capture_nRF(gr.top_block, Qt.QWidget):
+
+class capture_nRF(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "capture_nRF", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("capture_nRF")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "capture_nRF")
-
-        try:
-            geometry = self.settings.value("geometry")
-            if geometry:
-                self.restoreGeometry(geometry)
-        except BaseException as exc:
-            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
         self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 10000e3
-        self.centre_freq = centre_freq = 2425e6
+        self.centre_freq = centre_freq = 2423e6
 
         ##################################################
         # Blocks
@@ -83,7 +54,7 @@ class capture_nRF(gr.top_block, Qt.QWidget):
         self.iio_pluto_source_0.set_frequency(int(centre_freq))
         self.iio_pluto_source_0.set_samplerate(int(samp_rate))
         self.iio_pluto_source_0.set_gain_mode(0, 'manual')
-        self.iio_pluto_source_0.set_gain(0, 40)
+        self.iio_pluto_source_0.set_gain(0, 30)
         self.iio_pluto_source_0.set_quadrature(True)
         self.iio_pluto_source_0.set_rfdc(True)
         self.iio_pluto_source_0.set_bbdc(True)
@@ -102,14 +73,6 @@ class capture_nRF(gr.top_block, Qt.QWidget):
         self.connect((self.iio_pluto_source_0, 0), (self.blocks_correctiq_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.blocks_head_0, 0))
 
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "capture_nRF")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -131,30 +94,22 @@ class capture_nRF(gr.top_block, Qt.QWidget):
 
 
 def main(top_block_cls=capture_nRF, options=None):
-
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-
-    tb.start()
-    tb.flowgraph_started.set()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
+    tb.flowgraph_started.set()
 
-    qapp.exec_()
+    tb.wait()
+
 
 if __name__ == '__main__':
     main()
