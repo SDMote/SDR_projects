236be523c5835a47c8156b5424ec92574cbfd62b
Zimrahin <diegobadillosj@gmail.com>
2025-02-11 16:48:46 +0100

    OQPSK coherent: GUI cleaning

--- GNU_Radio_Companion/13_802154_coherent/OQPSK_coherent.py
+++ GNU_Radio_Companion/13_802154_coherent/OQPSK_coherent.py
@@ -12,7 +12,6 @@ from PyQt5 import Qt
 from gnuradio import qtgui
 from PyQt5 import QtCore
 from gnuradio import analog
-import math
 from gnuradio import blocks
 import pmt
 from gnuradio import digital
@@ -26,6 +25,7 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+import math
 import numpy as np
 import sip
 import threading
@@ -72,7 +72,7 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
         self.sps = sps = int(4)
         self.samp_rate = samp_rate = int(4e6)
         self.plot_N = plot_N = 50000
-        self.offset = offset = 0
+        self.offset = offset = -20000
         self.fsk_deviation_hz = fsk_deviation_hz = int(500e3)
         self.decimation = decimation = 1
         self.constellation = constellation = digital.constellation_qpsk().base()
@@ -90,54 +90,18 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self._offset_range = qtgui.Range(-500e3, 500e3, 1e3, 0, 200)
+        self._offset_range = qtgui.Range(-500e3, 500e3, 1e3, -20000, 200)
         self._offset_win = qtgui.RangeWidget(self._offset_range, self.set_offset, "'offset'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._offset_win, 6, 0, 1, 1)
         for r in range(6, 7):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self.single_pole_iir_filter_xx_0 = filter.single_pole_iir_filter_ff((160E-6), 1)
         self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
                 interpolation=2,
                 decimation=5,
                 taps=[],
                 fractional_bw=0)
-        self.qtgui_waterfall_sink_x_0_0_0 = qtgui.waterfall_sink_c(
-            1024, #size
-            window.WIN_HAMMING, #wintype
-            0, #fc
-            (samp_rate / decimation), #bw
-            "", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_waterfall_sink_x_0_0_0.set_update_time(0.10)
-        self.qtgui_waterfall_sink_x_0_0_0.enable_grid(False)
-        self.qtgui_waterfall_sink_x_0_0_0.enable_axis_labels(True)
-
-
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        colors = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_waterfall_sink_x_0_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_waterfall_sink_x_0_0_0.set_line_label(i, labels[i])
-            self.qtgui_waterfall_sink_x_0_0_0.set_color_map(i, colors[i])
-            self.qtgui_waterfall_sink_x_0_0_0.set_line_alpha(i, alphas[i])
-
-        self.qtgui_waterfall_sink_x_0_0_0.set_intensity_range(-140, 10)
-
-        self._qtgui_waterfall_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0_0_0.qwidget(), Qt.QWidget)
-
-        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_0_0_win)
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
             2048, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -172,7 +136,120 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
 
         self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
 
-        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win, 0, 1, 1, 1)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(1, 2):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0 = qtgui.time_sink_c(
+            (int(plot_N * 2 / 5 /4)), #size
+            samp_rate/sps, #samp_rate
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_y_axis(-2, 2)
+
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.enable_tags(True)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.enable_grid(False)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.enable_stem_plot(False)
+
+
+        labels = ['Receive after Costas Re', 'Receive after Costas Im', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [0, 0, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                if (i % 2 == 0):
+                    self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_0_0_0_0_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0_0_0_0_0_0.qwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_0_0_0_0_0_0_win, 1, 1, 1, 1)
+        for r in range(1, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(1, 2):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_eye_sink_x_0_0_0_0 = qtgui.eye_sink_c(
+            1024, #size
+            samp_rate/sps, #samp_rate
+            1, #number of inputs
+            None
+        )
+        self.qtgui_eye_sink_x_0_0_0_0.set_update_time(0.10)
+        self.qtgui_eye_sink_x_0_0_0_0.set_samp_per_symbol(1)
+        self.qtgui_eye_sink_x_0_0_0_0.set_y_axis(-2, 2)
+
+        self.qtgui_eye_sink_x_0_0_0_0.set_y_label('Amplitude', "")
+
+        self.qtgui_eye_sink_x_0_0_0_0.enable_tags(True)
+        self.qtgui_eye_sink_x_0_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_eye_sink_x_0_0_0_0.enable_autoscale(False)
+        self.qtgui_eye_sink_x_0_0_0_0.enable_grid(False)
+        self.qtgui_eye_sink_x_0_0_0_0.enable_axis_labels(True)
+        self.qtgui_eye_sink_x_0_0_0_0.enable_control_panel(False)
+
+
+        labels = ['Eye Diagram after Costas Re', 'Eye Diagram after Costas  Im', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'blue', 'blue', 'blue', 'blue',
+            'blue', 'blue', 'blue', 'blue', 'blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [0, 0, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                if (i % 2 == 0):
+                    self.qtgui_eye_sink_x_0_0_0_0.set_line_label(i, "Eye [Re{{Data {0}}}]".format(round(i/2)))
+                else:
+                    self.qtgui_eye_sink_x_0_0_0_0.set_line_label(i, "Eye [Im{{Data {0}}}]".format(round((i-1)/2)))
+            else:
+                self.qtgui_eye_sink_x_0_0_0_0.set_line_label(i, labels[i])
+            self.qtgui_eye_sink_x_0_0_0_0.set_line_width(i, widths[i])
+            self.qtgui_eye_sink_x_0_0_0_0.set_line_color(i, colors[i])
+            self.qtgui_eye_sink_x_0_0_0_0.set_line_style(i, styles[i])
+            self.qtgui_eye_sink_x_0_0_0_0.set_line_marker(i, markers[i])
+            self.qtgui_eye_sink_x_0_0_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_eye_sink_x_0_0_0_0_win = sip.wrapinstance(self.qtgui_eye_sink_x_0_0_0_0.qwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_eye_sink_x_0_0_0_0_win, 1, 0, 1, 1)
+        for r in range(1, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self.qtgui_const_sink_x_0_0_0 = qtgui.const_sink_c(
             1024, #size
             "", #name
@@ -181,7 +258,7 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
         )
         self.qtgui_const_sink_x_0_0_0.set_update_time(0.10)
         self.qtgui_const_sink_x_0_0_0.set_y_axis((-2), 2)
-        self.qtgui_const_sink_x_0_0_0.set_x_axis((-2), 2)
+        self.qtgui_const_sink_x_0_0_0.set_x_axis((-4), 4)
         self.qtgui_const_sink_x_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
         self.qtgui_const_sink_x_0_0_0.enable_autoscale(False)
         self.qtgui_const_sink_x_0_0_0.enable_grid(False)
@@ -213,8 +290,8 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
             self.qtgui_const_sink_x_0_0_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_const_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0_0.qwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_0_0_win, 17, 0, 1, 1)
-        for r in range(17, 18):
+        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_0_0_win, 0, 0, 1, 1)
+        for r in range(0, 1):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
@@ -230,19 +307,17 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
         self.digital_symbol_sync_xx_1 = digital.symbol_sync_cc(
             digital.TED_GARDNER,
             sps,
-            0.045,
+            (0.045/2),
             1.0,
             1.0,
-            1.5,
+            0.1,
             1,
             digital.constellation_bpsk().base(),
-            digital.IR_MMSE_8TAP,
-            128,
-            [])
+            digital.IR_PFB_NO_MF,
+            64,
+            np.sin(np.linspace(0, np.pi, sps + 1)) / np.sum(np.sin(np.linspace(0, np.pi, sps + 1))))
         self.digital_costas_loop_cc_0_0 = digital.costas_loop_cc((2*math.pi/50), constellation.arity(), False)
-        self.blocks_vco_c_0 = blocks.vco_c(samp_rate, (-2*math.pi*500e3), 1)
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, (samp_rate/40), True, 0 if "auto" == "auto" else max( int(float(0.1) * (samp_rate/40)) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_multiply_xx_0_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         self.blocks_float_to_complex_0.set_min_output_buffer((2**16))
@@ -252,16 +327,13 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
         self.analog_simple_squelch_cc_0 = analog.simple_squelch_cc((-50), 1)
         self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, offset, 1, 0, 0)
-        self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(((samp_rate / decimation)/(2*math.pi*fsk_deviation_hz)))
         self.analog_agc_xx_1_0 = analog.agc_cc((1e-4), 1.0, 1.0, 65536)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_agc_xx_1_0, 0), (self.blocks_multiply_xx_0_0_0, 0))
         self.connect((self.analog_agc_xx_1_0, 0), (self.digital_symbol_sync_xx_1, 0))
-        self.connect((self.analog_quadrature_demod_cf_0, 0), (self.single_pole_iir_filter_xx_0, 0))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 1))
         self.connect((self.analog_simple_squelch_cc_0, 0), (self.low_pass_filter_0_0, 0))
         self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_delay_0, 0))
@@ -269,18 +341,16 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_delay_0, 0), (self.blocks_float_to_complex_0, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self.analog_agc_xx_1_0, 0))
-        self.connect((self.blocks_multiply_xx_0_0, 0), (self.analog_quadrature_demod_cf_0, 0))
         self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_complex_to_float_0, 0))
         self.connect((self.blocks_multiply_xx_0_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
-        self.connect((self.blocks_multiply_xx_0_0_0, 0), (self.qtgui_waterfall_sink_x_0_0_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.blocks_vco_c_0, 0), (self.blocks_multiply_xx_0_0_0, 1))
         self.connect((self.digital_costas_loop_cc_0_0, 0), (self.qtgui_const_sink_x_0_0_0, 1))
+        self.connect((self.digital_costas_loop_cc_0_0, 0), (self.qtgui_eye_sink_x_0_0_0_0, 0))
+        self.connect((self.digital_costas_loop_cc_0_0, 0), (self.qtgui_time_sink_x_0_0_0_0_0_0_0, 0))
         self.connect((self.digital_symbol_sync_xx_1, 0), (self.digital_costas_loop_cc_0_0, 0))
         self.connect((self.digital_symbol_sync_xx_1, 0), (self.qtgui_const_sink_x_0_0_0, 0))
         self.connect((self.low_pass_filter_0_0, 0), (self.blocks_multiply_xx_0_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.analog_simple_squelch_cc_0, 0))
-        self.connect((self.single_pole_iir_filter_xx_0, 0), (self.blocks_vco_c_0, 0))
 
 
     def closeEvent(self, event):
@@ -305,19 +375,21 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
         self.sps = sps
         self.blocks_delay_0.set_dly(int((int(self.sps/2))))
         self.digital_symbol_sync_xx_1.set_sps(self.sps)
-        self.fir_filter_xxx_0.set_taps(np.sin(np.linspace(0, np.pi, self.sps + 1)))
+        self.fir_filter_xxx_0.set_taps(np.sin(np.linspace(0, np.pi, self.sps + 1)) / np.sum(np.sin(np.linspace(0, np.pi, self.sps + 1))))
+        self.qtgui_eye_sink_x_0_0_0_0.set_samp_rate(self.samp_rate/self.sps)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_samp_rate(self.samp_rate/self.sps)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_quadrature_demod_cf_0.set_gain(((self.samp_rate / self.decimation)/(2*math.pi*self.fsk_deviation_hz)))
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle2_0.set_sample_rate((self.samp_rate/40))
         self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, (self.tuning_LPF_cutoff_kHz*1000), (self.samp_rate/100), window.WIN_HAMMING, 6.76))
+        self.qtgui_eye_sink_x_0_0_0_0.set_samp_rate(self.samp_rate/self.sps)
+        self.qtgui_time_sink_x_0_0_0_0_0_0_0.set_samp_rate(self.samp_rate/self.sps)
         self.qtgui_waterfall_sink_x_0.set_frequency_range(0, (self.samp_rate / self.decimation))
-        self.qtgui_waterfall_sink_x_0_0_0.set_frequency_range(0, (self.samp_rate / self.decimation))
 
     def get_plot_N(self):
         return self.plot_N
@@ -337,16 +409,13 @@ class OQPSK_coherent(gr.top_block, Qt.QWidget):
 
     def set_fsk_deviation_hz(self, fsk_deviation_hz):
         self.fsk_deviation_hz = fsk_deviation_hz
-        self.analog_quadrature_demod_cf_0.set_gain(((self.samp_rate / self.decimation)/(2*math.pi*self.fsk_deviation_hz)))
 
     def get_decimation(self):
         return self.decimation
 
     def set_decimation(self, decimation):
         self.decimation = decimation
-        self.analog_quadrature_demod_cf_0.set_gain(((self.samp_rate / self.decimation)/(2*math.pi*self.fsk_deviation_hz)))
         self.qtgui_waterfall_sink_x_0.set_frequency_range(0, (self.samp_rate / self.decimation))
-        self.qtgui_waterfall_sink_x_0_0_0.set_frequency_range(0, (self.samp_rate / self.decimation))
 
     def get_constellation(self):
         return self.constellation
