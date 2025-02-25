b254221a911d6aa7d1d64bbfc496666e692d6dc8
Zimrahin <diegobadillosj@gmail.com>
2025-02-25 14:51:47 +0100

    BLE-BLE and 15.4-15.4 adjacent channel, time interference

--- capture_nRF/read_files.py
+++ capture_nRF/read_files.py
@@ -23,13 +23,14 @@ files = [
     "802154_8dBm_0MHz", # 15 ------------ IEEE 802.15.4 interference alone
     "BLE_tone_0dBm_8dBm_0MHz", # 16 ----- BLE + tone interference
     "tone_8dBm_0MHz_BLE", # 17 ------- tone interference alone
-    "802154_BLE_0dBm", # 18 ----  IEEE 802.15.4 alone -------------------
+    "802154_0dBm", # 18 ----  IEEE 802.15.4 alone -------------------
     "802154_BLE_0dBm_8dBm_0MHz", # 19 --- IEEE 802.15.4 + BLE interference
     "BLE_8dBm_0MHz", # 20 --------------- BLE interference alone
     "802154_tone_0dBm_8dBm_0MHz", # 21 -- IEEE 802.15.4 + tone interference
     "tone_8dBm_0MHz_802154", # 22 ---------- tone interference alone
-    "BLE_BLE_2424MHz_2426MHz" # 23 --- double BLE -----------------------
-    "802154_802154_2425MHz_2430MHz" # 24 --- double 802154 --------------
+    "BLE_BLE_2424MHz_2426MHz", # 23 --- double BLE -----------------------
+    "802154_802154_2425MHz_2430MHz", # 24 --- double 802154 --------------
+    "nrf_IQ" # -1
 ]
 
 
--- capture_nRF/read_nRF_IQ.py
+++ capture_nRF/read_nRF_IQ.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 # Load complex binary file
 data = np.fromfile("data/nrf_IQ.dat", dtype=np.complex64)
 
-start_index = 110000
-end_index = 120000
+start_index = 15
+end_index = 65
+start_index *= 1000
+end_index *= 1000
+
 sliced_data = data[start_index:end_index]
 
 
@@ -28,13 +31,13 @@ files = [
     "802154_8dBm_0MHz", # 15 ------------ IEEE 802.15.4 interference alone
     "BLE_tone_0dBm_8dBm_0MHz", # 16 ----- BLE + tone interference
     "tone_8dBm_0MHz_BLE", # 17 ------- tone interference alone
-    "802154_BLE_0dBm", # 18 ----  IEEE 802.15.4 alone -------------------
+    "802154_0dBm", # 18 ----  IEEE 802.15.4 alone -------------------
     "802154_BLE_0dBm_8dBm_0MHz", # 19 --- IEEE 802.15.4 + BLE interference
     "BLE_8dBm_0MHz", # 20 --------------- BLE interference alone
     "802154_tone_0dBm_8dBm_0MHz", # 21 -- IEEE 802.15.4 + tone interference
     "tone_8dBm_0MHz_802154", # 22 ---------- tone interference alone
-    "BLE_BLE_2424MHz_2426MHz" # 23 --- double BLE -----------------------
-    "802154_802154_2425MHz_2430MHz" # 24 --- double 802154 --------------
+    "BLE_BLE_2424MHz_2426MHz", # 23 --- double BLE -----------------------
+    "802154_802154_2425MHz_2430MHz", # 24 --- double 802154 --------------
 ]
 data_number = 13
 
