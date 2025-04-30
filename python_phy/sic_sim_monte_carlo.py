09130139ff47544dd4dc0da446f344e4147dca8d
Zimrahin <diegobadillosj@gmail.com>
2025-04-30 11:03:03 +0200

    SIC simulator: Add comments to Monte Carlo .py file

--- python_phy/sic_sim_monte_carlo.py
+++ python_phy/sic_sim_monte_carlo.py
@@ -23,27 +23,27 @@ from data_io import sic_save_simulation
 @click.option("--num-trials", default=4, type=int, help="Number of Monte Carlo trials.")
 def run_simulation(protocol_high, protocol_low, ble_rate, payload_len_high, payload_len_low, num_trials):
     cfg = SimulationConfig(
-        sampling_rate=10e6,
-        protocol_high=protocol_high,
+        sampling_rate=10e6,  # Samples per second
+        protocol_high=protocol_high,  # BLE or IEEE 802.15.4
         protocol_low=protocol_low,
-        ble_rate=ble_rate,
-        amplitude_high=10 ** (-6 / 20),
-        amplitude_low=10 ** (-7 / 20),
-        snr_low_db=0,
-        freq_offset_range=range(-5000, 5000, 50),
-        fine_step=2,
-        fine_window=50,
-        payload_len_high=payload_len_high,
-        payload_len_low=payload_len_low,
-        sample_shift_range_low=(0, 1),
+        ble_rate=ble_rate,  # 1 Mb/s or 2 Mb/s
+        amplitude_high=10 ** (-6 / 20),  # Amplitude for higher-power signal
+        amplitude_low=10 ** (-7 / 20),  # Amplitude for lower-power signal
+        snr_low_db=0,  # SNR in (dB) relative to the lower power generated signal
+        freq_offset_range=range(-5000, 5000, 50),  # (Hz) For demodulation brute force search
+        fine_step=2,  # Step size (Hz) for the fine search
+        fine_window=50,  # Half-width (Hz) of the window around best coarse frequency
+        payload_len_high=payload_len_high,  # Bytes in high-power payload
+        payload_len_low=payload_len_low,  # Bytes in low-power payload
+        sample_shift_range_low=(0, 1),  # Range to randomly apply fractional delays on IQ data
         sample_shift_range_high=(200, 2200),
-        freq_high=None,
-        freq_low=None,
-        phase_high=None,
-        phase_low=None,
-        adc_bits=12,
-        adc_vmax=1.0,
-        padding=500,
+        freq_high=None,  # Fixed frequency offset for high-power signal, if None random in freq_offset_range
+        freq_low=None,  # Fixed frequency offset for low-power signal, if None random in freq_offset_range
+        phase_high=None,  # Fixed phase for high-power signal, if None random
+        phase_low=None,  # Fixed phase for low-power signal, if None random
+        adc_bits=12,  # ADC resolution in bits
+        adc_vmax=1.0,  # Maximum ADC input amplitude
+        padding=500,  # Zero-pad the generated signals before adding them to ensure equal length
     )
 
     high_power_db = -6  # dB, around 0.707 amplitude
